2e2a75ccdfd240f3a71475fee15247e61641379d
Sunjun Hwang <sunjun.software@gmail.com>
2025-05-15 02:24:51 +0900

    added feeds and sidebar

--- src/App.js
+++ src/App.js
@@ -3,8 +3,24 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      <h1>The Ikizuraibu Feed</h1>
+    <div class="App">
+      <div class="header">
+        <h1>The Ikizuraibu Feed</h1>
+      </div>
+      <div className="sidenav">
+        <a>Polka</a>
+        <a>Mai</a>
+      </div>
+      <div className="feeds-container">
+        <div className="feed">
+          <a className="twitter-timeline" data-theme="dark" href="https://twitter.com/polka_lion?ref_src=twsrc%5Etfw">Tweets by polka_lion</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+        </div>
+      </div>
+      <div className="feeds-container d-none">
+        <div className="feed">
+          <a className="twitter-timeline" data-theme="dark" href="https://twitter.com/My_Mai_Eld?ref_src=twsrc%5Etfw">Tweets by My_Mai_Eld</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+        </div>
+      </div>
     </div>
   );
 }
