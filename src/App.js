d684aaef04282fff888d96d1d388f0aace3445f4
Sunjun Hwang <sunjun.software@gmail.com>
2025-05-20 23:16:10 +0900

    all Twitter feeds added, does not rerender

--- src/App.js
+++ src/App.js
@@ -7,11 +7,75 @@ import Feed from "./Components/Feed";
 
 function App() {
   const [member, setMember] = useState("");
+  const [twitter, setTwitter] = useState();
 
   const changeFeed = (name) => {
-    // SetMember(name);
-    // console.log(name);
-  }
+    setTwitter();
+    setMember(name);
+    console.log(name);
+
+    if(name === "Polka"){
+      setTwitter(
+        <div className="feed">
+          <a className="twitter-timeline" data-theme="dark" href="https://twitter.com/polka_lion?ref_src=twsrc%5Etfw">Tweets by polka_lion</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+        </div>
+      );
+    }else if (name === "Mai"){
+      setTwitter(
+        <div className="feed">
+          <a className="twitter-timeline" data-theme="dark" href="https://twitter.com/My_Mai_Eld?ref_src=twsrc%5Etfw">Tweets by My_Mai_Eld</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+        </div>
+      );
+    }else if (name === "Akira"){
+      setTwitter(
+        <div className="feed">
+          <a class="twitter-timeline" data-theme="dark" href="https://twitter.com/G_Akky304250?ref_src=twsrc%5Etfw">Tweets by G_Akky304250</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+        </div>
+      );
+    }else if (name === "Hanabi"){
+      setTwitter(
+        <div className="feed">
+          <a class="twitter-timeline" data-theme="dark" href="https://twitter.com/hanabistarmine?ref_src=twsrc%5Etfw">Tweets by hanabistarmine</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+        </div>
+      );
+    }else if (name === "Miracle"){
+      setTwitter(
+        <div className="feed">
+          <a class="twitter-timeline" data-theme="dark" href="https://twitter.com/MiracleGoldSP?ref_src=twsrc%5Etfw">Tweets by MiracleGoldSP</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+        </div>
+      );
+    }else if (name === "Noriko"){
+      setTwitter(
+        <div className="feed">
+          <a class="twitter-timeline" data-theme="dark" href="https://twitter.com/Noricco_U?ref_src=twsrc%5Etfw">Tweets by Noricco_U</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+        </div>
+      );
+    }else if (name === "Yukuri"){
+      setTwitter(
+        <div className="feed">
+          <a class="twitter-timeline" data-theme="dark" href="https://twitter.com/Yukuri_talk?ref_src=twsrc%5Etfw">Tweets by Yukuri_talk</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+        </div>
+      );
+    }else if (name === "Aurora"){
+      setTwitter(
+        <div className="feed">
+          <a class="twitter-timeline" data-theme="dark" href="https://twitter.com/Rollie_twinkle?ref_src=twsrc%5Etfw">Tweets by Rollie_twinkle</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+        </div>
+      );
+    }else if (name === "Midori"){
+      setTwitter(
+        <div className="feed">
+          <a class="twitter-timeline" data-theme="dark" href="https://twitter.com/LittlegreenCom?ref_src=twsrc%5Etfw">Tweets by LittlegreenCom</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+        </div>
+      );
+    }else if (name === "Shion"){
+      setTwitter(
+        <div className="feed">
+          <a class="twitter-timeline" data-theme="dark" href="https://twitter.com/ShaunTheBunny?ref_src=twsrc%5Etfw">Tweets by ShaunTheBunny</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+        </div>
+      );
+    }
+ }
 
   return (
     <div class="App">
@@ -19,69 +83,21 @@ function App() {
         <h1>The Ikizuraibu Feed</h1>
       </div>
       <div className="sidenav">
-        <a onClick={() => setMember("Polka")}>Polka</a>
-        <a onClick={() => setMember("Mai")}>Mai</a>
-        <a>Akira</a>
-        <a>Hanabi</a>
-        <a>Miracle</a>
-        <a>Noriko</a>
-        <a>Yukuri</a>
-        <a>Aurora</a>
-        <a>Midori</a>
-        <a>Shion</a>
+        <a onClick={() => changeFeed("Polka")}>Polka</a>
+        <a onClick={() => changeFeed("Mai")}>Mai</a>
+        <a onClick={() => changeFeed("Akira")}>Akira</a>
+        <a onClick={() => changeFeed("Hanabi")}>Hanabi</a>
+        <a onClick={() => changeFeed("Miracle")}>Miracle</a>
+        <a onClick={() => changeFeed("Noriko")}>Noriko</a>
+        <a onClick={() => changeFeed("Yukuri")}>Yukuri</a>
+        <a onClick={() => changeFeed("Aurora")}>Aurora</a>
+        <a onClick={() => changeFeed("Midori")}>Midori</a>
+        <a onClick={() => changeFeed("Shion")}>Shion</a>
       </div>
-      <Feed
-        member={member}
-        />
+      <p>{member}</p>
+
       <div className="feeds-container">
-        <div className="feed">
-          <a className="twitter-timeline" data-theme="dark" href="https://twitter.com/polka_lion?ref_src=twsrc%5Etfw">Tweets by polka_lion</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-        </div>
-      </div>
-      <div className="feeds-container d-none">
-        <div className="feed">
-          <a className="twitter-timeline" data-theme="dark" href="https://twitter.com/My_Mai_Eld?ref_src=twsrc%5Etfw">Tweets by My_Mai_Eld</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-        </div>
-      </div>
-      <div className="feeds-container d-none">
-        <div className="feed">
-          <a class="twitter-timeline" data-theme="dark" href="https://twitter.com/G_Akky304250?ref_src=twsrc%5Etfw">Tweets by G_Akky304250</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-        </div>
-      </div>
-      <div className="feeds-container d-none">
-        <div className="feed">
-          <a class="twitter-timeline" data-theme="dark" href="https://twitter.com/hanabistarmine?ref_src=twsrc%5Etfw">Tweets by hanabistarmine</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-        </div>
-      </div>
-      <div className="feeds-container d-none">
-        <div className="feed">
-          <a class="twitter-timeline" data-theme="dark" href="https://twitter.com/MiracleGoldSP?ref_src=twsrc%5Etfw">Tweets by MiracleGoldSP</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-        </div>
-      </div>
-      <div className="feeds-container d-none">
-        <div className="feed">
-          <a class="twitter-timeline" data-theme="dark" href="https://twitter.com/Noricco_U?ref_src=twsrc%5Etfw">Tweets by Noricco_U</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-        </div>
-      </div>
-      <div className="feeds-container d-none">
-        <div className="feed">
-          <a class="twitter-timeline" data-theme="dark" href="https://twitter.com/Yukuri_talk?ref_src=twsrc%5Etfw">Tweets by Yukuri_talk</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-        </div>
-      </div>
-      <div className="feeds-container d-none">
-        <div className="feed">
-          <a class="twitter-timeline" data-theme="dark" href="https://twitter.com/Rollie_twinkle?ref_src=twsrc%5Etfw">Tweets by Rollie_twinkle</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-        </div>
-      </div>
-      <div className="feeds-container d-none">
-        <div className="feed">
-          <a class="twitter-timeline" data-theme="dark" href="https://twitter.com/LittlegreenCom?ref_src=twsrc%5Etfw">Tweets by LittlegreenCom</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-        </div>
-      </div>
-      <div className="feeds-container d-none">
-        <div className="feed">
-          <a class="twitter-timeline" data-theme="dark" href="https://twitter.com/ShaunTheBunny?ref_src=twsrc%5Etfw">Tweets by ShaunTheBunny</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-        </div>
+        {twitter}
       </div>
     </div>
   );
--- src/Components/Feed.js
+++ src/Components/Feed.js
@@ -3,11 +3,25 @@ import React, { Component } from "react";
 class Feed extends Component {
   render(){
     const { member } = this.props;
+    let twitter = "monkey";
+
+    if({member} == 'Polka'){
+      twitter = "Polka";
+        // <div>
+        //   monkey
+        //   <a className="twitter-timeline" data-theme="dark" href="https://twitter.com/polka_lion?ref_src=twsrc%5Etfw">Tweets by polka_lion</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+        // </div>
+    }else{
+      console.log({member});
+    }
 
     return(
 
-      <div>
-        <p>{member}</p>
+      <div className="feeds-container">
+        <div className="feed">
+          <p>{member}</p>
+          {twitter}
+        </div>
       </div>
     )
   }
